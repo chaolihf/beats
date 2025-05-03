936cc6b8fb8001f6c7cefd7fc906fb618ab39656
chaolihf <lichao01>
2025-05-03 23:25:15 +0800

    build: 更新 Go 依赖至 v1.24.2
    - 将 Go 语言版本从 1.21 升级到 1.24.2
    - 更新多个依赖库到最新版本，包括：
      - cloud.google.com/go
      - github.com/Azure
      - github.com/aws
      - github.com/elastic
      - golang.org/x
      - google.golang.org
      - k8s.io
      - sigs.k8s.io
    - 移除了一些旧的依赖库版本

--- libbeat/plugin/plugin_dlopen.go
+++ libbeat/plugin/plugin_dlopen.go
@@ -36,95 +36,10 @@ import "C"
 import (
 	"errors"
 	"sync"
-	"unsafe"
 )
 
 func open(name string) (*Plugin, error) {
-	cPath := make([]byte, C.PATH_MAX+1)
-	cRelName := make([]byte, len(name)+1)
-	copy(cRelName, name)
-	if C.realpath(
-		(*C.char)(unsafe.Pointer(&cRelName[0])),
-		(*C.char)(unsafe.Pointer(&cPath[0]))) == nil {
-		return nil, errors.New(`plugin.Open("` + name + `"): realpath failed`)
-	}
-
-	filepath := C.GoString((*C.char)(unsafe.Pointer(&cPath[0])))
-
-	pluginsMu.Lock()
-	if p := sysPlugins[filepath]; p != nil {
-		pluginsMu.Unlock()
-		if p.err != "" {
-			return nil, errors.New(`plugin.Open("` + name + `"): ` + p.err + ` (previous failure)`)
-		}
-		<-p.loaded
-		return p, nil
-	}
-	var cErr *C.char
-	h := C.pluginOpen((*C.char)(unsafe.Pointer(&cPath[0])), &cErr)
-	if h == 0 {
-		pluginsMu.Unlock()
-		return nil, errors.New(`plugin.Open("` + name + `"): ` + C.GoString(cErr))
-	}
-	// TODO(crawshaw): look for plugin note, confirm it is a Go plugin
-	// and it was built with the correct toolchain.
-	if len(name) > 3 && name[len(name)-3:] == ".so" {
-		name = name[:len(name)-3]
-	}
-	if sysPlugins == nil {
-		sysPlugins = make(map[string]*Plugin)
-	}
-	pluginpath, syms, initTasks, errstr := lastmoduleinit()
-	if errstr != "" {
-		sysPlugins[filepath] = &Plugin{
-			pluginpath: pluginpath,
-			err:        errstr,
-		}
-		pluginsMu.Unlock()
-		return nil, errors.New(`plugin.Open("` + name + `"): ` + errstr)
-	}
-	// This function can be called from the init function of a plugin.
-	// Drop a placeholder in the map so subsequent opens can wait on it.
-	p := &Plugin{
-		pluginpath: pluginpath,
-		loaded:     make(chan struct{}),
-	}
-	sysPlugins[filepath] = p
-	pluginsMu.Unlock()
-
-	doInit(initTasks)
-
-	// Fill out the value of each plugin symbol.
-	updatedSyms := map[string]any{}
-	for symName, sym := range syms {
-		isFunc := symName[0] == '.'
-		if isFunc {
-			delete(syms, symName)
-			symName = symName[1:]
-		}
-
-		fullName := pluginpath + "." + symName
-		cname := make([]byte, len(fullName)+1)
-		copy(cname, fullName)
-
-		p := C.pluginLookup(h, (*C.char)(unsafe.Pointer(&cname[0])), &cErr)
-		if p == nil {
-			return nil, errors.New(`plugin.Open("` + name + `"): could not find symbol ` + symName + `: ` + C.GoString(cErr))
-		}
-		valp := (*[2]unsafe.Pointer)(unsafe.Pointer(&sym))
-		if isFunc {
-			(*valp)[1] = unsafe.Pointer(&p)
-		} else {
-			(*valp)[1] = p
-		}
-		// we can't add to syms during iteration as we'll end up processing
-		// some symbols twice with the inability to tell if the symbol is a function
-		updatedSyms[symName] = sym
-	}
-	p.syms = updatedSyms
-
-	close(p.loaded)
-	return p, nil
+	return nil, nil
 }
 
 func lookup(p *Plugin, symName string) (Symbol, error) {
@@ -140,7 +55,7 @@ var (
 )
 
 // lastmoduleinit is defined in package runtime.
-func lastmoduleinit() (pluginpath string, syms map[string]any, inittasks []*initTask, errstr string)
+//func lastmoduleinit() (pluginpath string, syms map[string]any, inittasks []*initTask, errstr string)
 
 // doInit is defined in package runtime.
 //
